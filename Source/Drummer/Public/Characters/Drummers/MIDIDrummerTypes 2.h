132912c8fc9e3291b2edd64ff93cdf9989443017
Olav Utne Skjeldal <olav.skjeldal@moenmarin.no>
2024-12-03 11:31:58 +0100

    Random Update

---
+++ Source/Drummer/Public/Characters/Drummers/MIDIDrummerTypes 2.h
@@ -0,0 +1,15 @@
+#pragma once
+
+UENUM(BlueprintType)
+enum class EMIDIDrummerState : uint8
+{
+    EDS_Idle UMETA(DisplayName = "Idle"),
+    EDS_Drumming UMETA(DisplayName = "Drumming"),
+};
+
+UENUM(BlueprintType)
+enum class EMIDIDrummerActionState : uint8
+{
+    EDAS_Unoccupied UMETA(DisplayName = "Unoccupied"),
+    EDAS_Moving UMETA(DisplayName = "Moving"),
+};
